22b6f78f0c3930e2b42124ab278a438027c20471
Altraoz <curresty@unal.edu.co>
2021-10-26 20:29:15 -0500

    tercer commit
    xd

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ class Asiento:
     def __init__(self, color, precio, registro):
         self.color = color
         self.precio = precio
-        self.regitro = registro
+        self.registro = registro
     
     def cambiarColor(self, color):
         if (color == "rojo" or color =="verde" or color =="amarillo" or color =="negro" or color =="blanco"):
